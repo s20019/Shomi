81da7a155f4813f0aaab22bbbc81dd112bf6bc50
s20019 <s20019@std.it-college.ac.jp>
2022-08-10 11:20:16 +0900

    サインアップ画面とサインイン画面の追加
    SignInActivityとSignUpActivityの追加とそれに伴う変更

---
+++ app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s20019.expirydatemanager2.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.it_college.std.s20019.expirydatemanager2.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
