35fe0dc6f47283d2cd63f31ea6b523770463ca5b
s20019 <s20019@std.it-college.ac.jp>
2022-08-17 12:02:00 +0900

    メニュー項目のカスタマイズをタップした時の処理を追加
    onOptionsItemSelected(), onPrepareOptionsMenu()の追加

--- app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package jp.ac.it_college.std.s20019.expirydatemanager2.activity
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
@@ -25,14 +27,61 @@ class MainActivity : AppCompatActivity() {
         // 常にダークテーマをOFFにする処理
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
 
-
         notification()
 
     }
 
+    // アプリバーにメニューアイコンを表示する処理
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
+
         return true
+    }
+
+    // それぞれのメニューがタップされた時の処理
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_customize -> {
+                binding.imgDeleteBtn1.visibility = View.VISIBLE
+                binding.imgDeleteBtn2.visibility = View.VISIBLE
+                binding.imgDeleteBtn3.visibility = View.VISIBLE
+                binding.imgDeleteBtn4.visibility = View.VISIBLE
+                binding.imgDeleteBtn5.visibility = View.VISIBLE
+                true
+            }
+            R.id.action_end -> {
+                binding.imgDeleteBtn1.visibility = View.INVISIBLE
+                binding.imgDeleteBtn2.visibility = View.INVISIBLE
+                binding.imgDeleteBtn3.visibility = View.INVISIBLE
+                binding.imgDeleteBtn4.visibility = View.INVISIBLE
+                binding.imgDeleteBtn5.visibility = View.INVISIBLE
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    // メニュー項目を表示する前の調整
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        // 削除ボタンが表示されている場合は「終了」項目のみ表示
+        if (binding.imgDeleteBtn1.visibility == View.VISIBLE) {
+            menu?.findItem(R.id.action_customize)?.isVisible = false
+            menu?.findItem(R.id.action_profile)?.isVisible = false
+            menu?.findItem(R.id.action_friend)?.isVisible = false
+            menu?.findItem(R.id.action_settings)?.isVisible = false
+
+            menu?.findItem(R.id.action_end)?.isVisible = true
+        }
+        // 削除ボタンが表示されていなければデフォルトの項目を表示
+        else {
+            menu?.findItem(R.id.action_customize)?.isVisible = true
+            menu?.findItem(R.id.action_profile)?.isVisible = true
+            menu?.findItem(R.id.action_friend)?.isVisible = true
+            menu?.findItem(R.id.action_settings)?.isVisible = true
+
+            menu?.findItem(R.id.action_end)?.isVisible = false
+        }
+        return super.onPrepareOptionsMenu(menu)
     }
 
 
