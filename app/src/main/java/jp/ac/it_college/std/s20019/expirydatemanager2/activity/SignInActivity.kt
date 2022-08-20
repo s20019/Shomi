1121fc88fc4aafa5282d1a677cc67d47cc3158d2
s20019 <s20019@std.it-college.ac.jp>
2022-08-21 01:27:41 +0900

    サインイン画面を追加

--- app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/SignInActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/SignInActivity.kt
@@ -1,12 +1,44 @@
 package jp.ac.it_college.std.s20019.expirydatemanager2.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import jp.ac.it_college.std.s20019.expirydatemanager2.R
+import jp.ac.it_college.std.s20019.expirydatemanager2.databinding.ActivitySignInBinding
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), TextWatcher {
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 「今すぐ登録」を押したときの処理
+        binding.registerNowText.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.logInBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("USER_NAME", binding.userName.text.toString())
+            startActivity(intent)
+        }
+    }
+
+    // 文字列が修正されたあと二呼び出されるメソッド
+    override fun afterTextChanged(p0: Editable?) { }
+
+    // 文字列が修正される直前に呼び出されるメソッド
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+    // 文字１つを入力したときに呼び出されるメソッド
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        binding.personImg.visibility = View.GONE
+        binding.keyImg.visibility = View.GONE
     }
 }
