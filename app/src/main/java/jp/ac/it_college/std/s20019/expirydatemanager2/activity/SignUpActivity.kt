b91a2e1d35a9b184451266c01e0a3b53ae382dd4
s20019 <s20019@std.it-college.ac.jp>
2022-08-21 01:27:53 +0900

    サインアップ画面を追加

--- app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/SignUpActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20019/expirydatemanager2/activity/SignUpActivity.kt
@@ -1,12 +1,23 @@
 package jp.ac.it_college.std.s20019.expirydatemanager2.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import jp.ac.it_college.std.s20019.expirydatemanager2.R
+import jp.ac.it_college.std.s20019.expirydatemanager2.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.moveOnBtn.setOnClickListener {
+            val intent = Intent(this, SignUpConfirmActivity::class.java)
+            intent.putExtra("USER_NAME", binding.userNameSignUp.text.toString())
+            startActivity(intent)
+        }
     }
 }
